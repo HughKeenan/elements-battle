224d4079584e1e9cdd09d7d4506d88f750676cc6
Hugh Keenan <hugh.keenan114@gmail.com>
2024-05-07 19:33:08 +0000

    added javascript to govern interactions between options

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,28 @@ const options = ["water","earth","fire","air","steel"]
 const playerSelects = document.getElementById('playerSelects')
 const computerSelects = document.getElementById('computerSelects')
 
-function gameManager(){
-    let computerSelects = Math.floor(Math.random(options)*4)
-    
+function gameManager(playerChoice){
+    const computerSelects = options[Math.floor(Math.random()*5)]
+    console.log(computerSelects)
+    if (playerChoice === computerSelects){
+        alert("It's a draw!")
+    }else{
+    switch (playerChoice){
+        case 'water':
+            result = (computerSelects === 'fire') ? alert('You Win!') : alert('You Lose!')
+            result = (computerSelects === 'steel') ? alert('You Win!') : alert('You Lose!')
+        case 'fire':
+            result = (computerSelects === 'air') ? alert('You Win!') : alert('You Lose!')
+            result = (computerSelects === 'steel') ? alert('You Win!') : alert('You Lose!')
+        case 'earth':
+            result = (computerSelects === 'water') ? alert('You Win!') : alert('You Lose!')
+            result = (computerSelects === 'fire') ? alert('You Win!') : alert('You Lose!')
+        case 'air':
+            result = (computerSelects === 'water') ? alert('You Win!') : alert('You Lose!')
+            result = (computerSelects === 'earth') ? alert('You Win!') : alert('You Lose!')
+        case 'steel':
+        result = (computerSelects === 'air') ? alert('You Win!') : alert('You Lose!')
+        result = (computerSelects === 'earth') ? alert('You Win!') : alert('You Lose!')  
+    }
+}
 }
