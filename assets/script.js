9de59ba033386888cded22bf5ec83e2eab2b6505
Hugh Keenan <hugh.keenan114@gmail.com>
2024-05-09 17:37:36 +0000

    changed script governing display of images

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,52 @@
 const options = ["water","earth","fire","air","steel"]
 const playerSelects = document.getElementById('playerSelects')
 const computerSelects = document.getElementById('computerSelects')
+const questionmark = document.getElementById('questionmark')
+const water = document.getElementById('water')
+const hydro = document.getElementById('hydro')
+const fire = document.getElementById('fire')
+const pyro = document.getElementById('pyro')
+const earth = document.getElementById('earth')
+const terra = document.getElementById('terra')
+const air = document.getElementById('air')
+const aero = document.getElementById('aero')
+const steel = document.getElementById('steel')
+const iron = document.getElementById('iron')
 
 function gameManager(playerChoice){
-    setPicture(playerChoice);
-
-    const computerSelects = options[Math.floor(Math.random()*5)]
-    console.log(computerSelects)
-    if (playerChoice === computerSelects){
-        alert("It's a draw!")
-    }else{
-    switch (playerChoice){
-        case 'water':
-            result = (computerSelects === 'fire' || computerSelects === 'steel') ? alert('You Win!') : alert('You Lose!');
-            break;
-        case 'fire':
-            result = (computerSelects === 'air' || computerSelects === 'steel') ? alert('You Win!') : alert('You Lose!');
-            break;
-        case 'earth':
-            result = (computerSelects === 'water' || computerSelects === 'fire') ? alert('You Win!') : alert('You Lose!');
-            break;
-        case 'air':
-            result = (computerSelects === 'water' || computerSelects === 'earth') ? alert('You Win!') : alert('You Lose!');
-            break;
-        case 'steel':
-        result = (computerSelects === 'air' || computerSelects === 'earth') ? alert('You Win!') : alert('You Lose!');
-            break;  
-    }
-}
-}
-
-function setPicture(playerChoice){
-    const questionmark = document.getElementById('questionmark')
-    const water = document.getElementById('water')
-    const fire = document.getElementById('fire')
-    const earth = document.getElementById('earth')
-    const air = document.getElementById('air')
-    const steel = document.getElementById('steel')
-
-    console.log(playerChoice)
-    
     if(playerChoice === 'water'){
         water.classList.remove('inactive')
         water.classList.add('active')
         questionmark.classList.remove('active')
         questionmark.classList.add('inactive')
-    }else if(playerChoice === 'fire'){
-        fire.classList.remove('inactive')
-        fire.classList.add('active')
-        questionmark.classList.remove('active')
-        questionmark.classList.add('inactive')
-    }else if(playerChoice === 'earth'){
-        earth.classList.remove('inactive')
-        earth.classList.add('active')
-        questionmark.classList.remove('active')
-        questionmark.classList.add('inactive')
-    }else if(playerChoice === 'air'){
-        air.classList.remove('inactive')
-        air.classList.add('active')
-        questionmark.classList.remove('active')
-        questionmark.classList.add('inactive')
-    }else if(playerChoice === 'steel'){
-        steel.classList.remove('inactive')
-        steel.classList.add('active')
-        questionmark.classList.remove('active')
-        questionmark.classList.add('inactive')
     }
-    
+
+    let computerSelects = options[Math.floor(Math.random()*5)]
+    console.log(computerSelects)
+    if (playerChoice === computerSelects){
+        console.log("It's a draw!")
+    }else{
+    switch (playerChoice){
+        case 'water':
+            result = (computerSelects === 'fire' || computerSelects === 'steel') ? console.log('You Win!') : console.log('You Lose!');
+            break;
+        case 'fire':
+            result = (computerSelects === 'air' || computerSelects === 'steel') ? console.log('You Win!') : console.log('You Lose!');
+            break;
+        case 'earth':
+            result = (computerSelects === 'water' || computerSelects === 'fire') ? console.log('You Win!') : console.log('You Lose!');
+            break;
+        case 'air':
+            result = (computerSelects === 'water' || computerSelects === 'earth') ? console.log('You Win!') : console.log('You Lose!');
+            break;
+        case 'steel':
+        result = (computerSelects === 'air' || computerSelects === 'earth') ? console.log('You Win!') : console.log('You Lose!');
+            break;  
+    }
+}
+}
+function setPicture(computerChoice){
     
 }
+
+
